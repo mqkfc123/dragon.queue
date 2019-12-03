f341da73abeeb60bab2413af7001385bc78f54a0
liuyl <mqkfc123@163.com>
2019-12-03 11:01:59 +0800

    net 40 版本

--- DQueue.RabbitMQ/Properties/AssemblyInfo.cs
+++ DQueue.RabbitMQ/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("DQueue.RabbitMQ")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("net40版本")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DQueue.RabbitMQ")]
